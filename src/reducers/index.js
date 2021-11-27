a249309b2a9fb29fe4f8aa71970ce0ffb5315a49
Het <het.thakkar809@gmail.com>
2021-11-27 00:33:46 -0500

    added redux devtools

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,14 @@
-
-import { combineReducers } from "redux";
-import auth from './auth';
-import search from './search';
-
-
-import job from './job';
+import { combineReducers } from 'redux'
+import auth from './auth'
+import search from './search'
+import job from './job'
 import application from './application'
+import utilState from './utilState'
 
 export default combineReducers({
     auth,
     search,
-    
-    
+    utilState,
     job,
-    application
+    application,
 })
-
