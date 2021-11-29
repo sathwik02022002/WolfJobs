dd08a3727ef1016950e50068c8ed2f49f9878052
dgupta23 <dgupta23@ncsu.edu>
2021-11-29 16:37:50 -0500

    Added test cases for Job.js

--- src/test/components/Job.test.js
+++ src/test/components/Job.test.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux'
+import Job from '../../components/Job';
+import {configureStore} from '../../store'
+
+const store = configureStore()
+
 test('Job', () => {
- const wrapper = shallow(<post />);
+ const wrapper = shallow(
+
+    <Provider store={store}>
+        <Job />
+    </Provider>
+    );
  expect(wrapper).toMatchSnapshot();
 });
