1f18251c57b8a8cafe4a2ca50c443ae621263001
dgupta23 <dgupta23@ncsu.edu>
2021-12-04 17:29:22 -0500

    Added signup.js test

--- src/test/components/signup.test.js
+++ src/test/components/signup.test.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-test('signup', () => {
- const wrapper = shallow(<login-form />);
- expect(wrapper).toMatchSnapshot();
+import { shallow, mount } from 'enzyme';
+import { Provider } from 'react-redux'
+import Signup from '../../components/Signup';
+import { configureStore } from '../../store'
+
+const store = configureStore()
+
+const error = {
+    
+};
+
+test('render', () => {
+    const wrapper = mount(
+        <Provider store={store}>
+            <Signup />
+        </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+
 });
+
