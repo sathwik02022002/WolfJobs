f34d35bf2158ad57b4a365183ae8911748cfa1f4
dgupta23 <dgupta23@ncsu.edu>
2021-12-04 17:12:29 -0500

    Added history.js test

---
+++ src/test/components/history.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { Provider } from 'react-redux'
+import History from '../../components/History';
+import { configureStore } from '../../store'
+
+const store = configureStore()
+
+const user = {
+    _id : "test"
+};
+
+const application = []
+
+test('render', () => {
+    const wrapper = mount(
+        <Provider store={store}>
+            <History user={user} application = {application}/>
+        </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+
+});
+
