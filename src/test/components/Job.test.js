6a7845c8127854922a59d991ca99028e9b90c11f
dgupta23 <dgupta23@ncsu.edu>
2021-12-04 14:38:31 -0500

    added job test

--- src/test/components/Job.test.js
+++ src/test/components/Job.test.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Provider } from 'react-redux'
-import Job from '../../components/Job';
-import {configureStore} from '../../store'
+import createJob from '../../actions/job';
+import jobSuccess from '../../actions/job';
+import fetchJobs from '../../actions/job';
 
-const store = configureStore()
-
-test('Job', () => {
- const wrapper = shallow(
-
-    <Provider store={store}>
-        <Job />
-    </Provider>
-    );
+test('createJob', () => {
+ const wrapper = shallow(<createJob/>);
  expect(wrapper).toMatchSnapshot();
 });
+
+test('jobSuccess', () => {
+    const wrapper = shallow(<jobSuccess/>);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('fetchJobs', () => {
+    const wrapper = shallow(<fetchJobs/>);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('createApplication', () => {
+    const wrapper = shallow(<createApplication/>);
+    expect(wrapper).toMatchSnapshot();
+});
+
