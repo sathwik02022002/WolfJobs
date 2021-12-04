4315d334c7697689482320e6daa02f3e3c0df0b1
dgupta23 <dgupta23@ncsu.edu>
2021-12-04 14:39:23 -0500

    added widget test

--- src/test/components/widgets.test.js
+++ src/test/components/widgets.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import Widgets from '../../components/Widgets';
+
 test('widgets', () => {
- const wrapper = shallow(<widgets />);
+ const wrapper = shallow(<Widgets />);
  expect(wrapper).toMatchSnapshot();
 });
