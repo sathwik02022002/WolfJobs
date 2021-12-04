6ab6dac153b6b86efc76e0852f58992547dfc40d
Het <het.thakkar809@gmail.com>
2021-12-04 16:32:27 -0500

    fix: tests

--- src/test/components/Job.test.js
+++ src/test/components/Job.test.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { Provider } from 'react-redux'
 import Job from '../../components/Job';
-import {configureStore} from '../../store'
+import { configureStore } from '../../store'
 
 const store = configureStore()
 
-test('Job', () => {
- const wrapper = shallow(
+const job = {
+    _id: 1,
+    name: 'test',
+    status: '0',
+    location: 'test',
+    description: 'test',
+    pay: 'test',
+    schedule: 'test',
+    managerid: 'test',
+};
 
-    <Provider store={store}>
-        <Job />
-    </Provider>
+test('Job', () => {
+    const wrapper = shallow(
+
+        <Provider store={store}>
+            <Job job={job} />
+        </Provider>
     );
- expect(wrapper).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('render', () => {
+    const wrapper = mount(
+        <Provider store={store}>
+            <Job job={job} />
+        </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
 });
