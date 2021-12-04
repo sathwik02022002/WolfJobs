6537f64d3f87e1e5d8b6280120a93216a0dfebfc
dgupta23 <dgupta23@ncsu.edu>
2021-12-04 14:38:42 -0500

    added login test

--- src/test/components/login.test.js
+++ src/test/components/login.test.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux'
+import Login from '../../components/Login';
+import {configureStore} from '../../store';
+
+const store = configureStore()
+
+
 test('login', () => {
- const wrapper = shallow(<login-form />);
+ const wrapper = shallow(
+    <Provider store={store}>
+    <Login />
+    </Provider>
+);
  expect(wrapper).toMatchSnapshot();
+ expect(wrapper.componentWillUnmount);
+ expect(wrapper.handleFormSubmit);
+ expect(wrapper.render);
 });
