38340808a1d404dd157d829659e2197542b23cbb
neelkanthtripathi <ntripat2@ncsu.edu>
2021-12-04 17:33:17 -0500

    Added settings test

--- src/test/components/settings.test.js
+++ src/test/components/settings.test.js
@@ -1,6 +1,42 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-test('settings', () => {
- const wrapper = shallow(<settings />);
- expect(wrapper).toMatchSnapshot();
+import { shallow, mount } from 'enzyme';
+import { Provider } from 'react-redux'
+import Settings from '../../components/Settings';
+import { configureStore } from '../../store'
+
+const store = configureStore()
+
+const state = {
+    name : "test",
+    password : "test",
+    confirmPassword : "test",
+    user_id : "test",
+    userrole : "test",
+    address: "test",
+    phonenumber: "test",
+    hours: "test",
+    dob: "test",
+    gender: "test",
+    skills: "test"
+}
+
+
+test('Settings', () => {
+    const wrapper = shallow(
+
+        <Provider store={store}>
+            <Settings state={state} />
+        </Provider>
+    );
 });
+
+test('render', () => {
+    const wrapper = mount(
+        <Provider store={store}>
+            <Settings state={state} />
+        </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+
+});
+
