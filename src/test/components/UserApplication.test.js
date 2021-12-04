4d7b25cbd908c19473ca9f0f4d00e248e5ed5f81
dgupta23 <dgupta23@ncsu.edu>
2021-12-04 14:39:14 -0500

    added user application test

--- src/test/components/UserApplication.test.js
+++ src/test/components/UserApplication.test.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import UserApplication from '../../components/UserApplication';
+import  {Provider}  from 'react-redux'
+import {configureStore} from '../../store'
+
+const store = configureStore()
+
+
+
 test('UserApplication', () => {
- const wrapper = shallow(<helloooooooooo />);
+ const wrapper = shallow(
+    <Provider store={store}>
+      <UserApplication />
+    </Provider> 
+ );
  expect(wrapper).toMatchSnapshot();
 });
