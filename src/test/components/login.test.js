8b8523a444cdd197962a904f04f97138d2626bab
neelkanthtripathi <ntripat2@ncsu.edu>
2021-12-04 16:59:38 -0500

    Adding login test

--- src/test/components/login.test.js
+++ src/test/components/login.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow ,mount} from 'enzyme';
 import { Provider } from 'react-redux'
 import Login from '../../components/Login';
 import {configureStore} from '../../store';
@@ -7,14 +7,31 @@ import {configureStore} from '../../store';
 const store = configureStore()
 
 
+
+const login = {
+    email:"test",
+    password:"test"
+}
+ 
+const location = {
+    state : "test"
+}
+
 test('login', () => {
  const wrapper = shallow(
     <Provider store={store}>
-    <Login />
+    <Login login={login} />
     </Provider>
-);
+ );
  expect(wrapper).toMatchSnapshot();
- expect(wrapper.componentWillUnmount);
- expect(wrapper.handleFormSubmit);
- expect(wrapper.render);
+
+ });
+test('render', () => {
+    const wrapper = mount(
+        <Provider store={store}>
+            <Login login={login} location = {location}/>
+        </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
 });
+
