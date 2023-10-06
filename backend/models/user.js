07afc86343e1d047b62fa889804e9df2101f7263
Deepak Rajendran <deepak.rajendran123@gmail.com>
2023-10-05 20:53:34 -0400

    added frontend and backend folder projects

---
+++ backend/models/user.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      default: '',
+    },
+    phonenumber: {
+      type: String,
+      default: '',
+    },
+    hours: {
+      type: String,
+      default: '',
+    },
+    dob: {
+      type: Date,
+    },
+    gender: {
+      type: String,
+      default: '',
+    },
+    availability: {
+      type: String,
+      default: '',
+    },
+    skills: {
+      type: Array,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
