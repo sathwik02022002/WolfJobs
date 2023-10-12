bd3324e6feb3a16011902b032b1ab6be479159e5
Deepak Rajendran <deepak.rajendran123@gmail.com>
2023-10-12 19:05:40 -0400

    updated fonts

--- frontend/src/feature/dashboard/components/dashboard.tsx
+++ frontend/src/feature/dashboard/components/dashboard.tsx
@@ -125,25 +125,29 @@ const JobListTile = () => {
       <div className="p-3 bg-white rounded-xl">
         <div className="flex flex-row">
           <div className="w-4/6 ">
-            <div className="w-fit bg-[#FF2A2A1A] rounded-full px-2 py-1 ">
-              <p className="inline" style={{ width: "fit-content" }}>
-                {/* <div className="inline"> */}
-                <div className="flex flex-row justify-center">
-                  <div className=" p-1 w-1  bg-[#FF2A2A1A]  rounded-full"></div>
-                </div>
-                {/* </div> */}
-                NC State Dining
+            <div className="w-fit bg-[#FF2A2A]/10 rounded-2xl px-3 py-0 ">
+              <p className="inline text-xs" style={{ width: "fit-content" }}>
+                {/* <div className="inline">
+                  <div className="flex flex-row justify-center">
+                    <div className=" p-1 w-1  bg-[#FF2A2A1A]  rounded-full"></div>
+                  </div>
+                </div> */}
+                {"NC State Dining".toUpperCase()}
               </p>
             </div>
-            <p>
-              <b>Role:</b> Dining Associate <br />
-              <b>Job Status:</b> Closed <br />
-              <b>Type:</b> Full-time <br />
+            <p className="text-sm">
+              <b>Role:</b> Dining Associate{" "}
+            </p>
+            <p className="text-sm">
+              <b>Job Status:</b> Closed{" "}
+            </p>
+            <p className="text-sm">
+              <b>Type:</b> Full-time
             </p>
           </div>
           <div className="w-2/6  flex flex-col-reverse text-right">
-            <p>Know more</p>
-            <p>40$/hr</p>
+            <p className="text-xs">Know more</p>
+            <p className="text-3xl">40$/hr</p>
           </div>
         </div>
       </div>
