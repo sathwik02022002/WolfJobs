56219f97782cb4b7c7132866a49e75ce24bb8c05
Janhavi Pendse <janhavip1231@gmail.com>
2023-10-19 23:06:31 -0400

    Changes in directory

--- frontendOld/src/actions/utilState.js
+++
@@ -1,8 +0,0 @@
-import { SET_LOADING } from "./actionTypes";
-
-export function setIsLoading(isLoading) {
-  return {
-    type: SET_LOADING,
-    isLoading,
-  };
-}
--- frontendOld/src/test/components/widgets.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import Widgets from "../../components/Widgets";
-
-test("widgets", () => {
-  const wrapper = shallow(<Widgets />);
-  expect(wrapper).toMatchSnapshot();
-});
