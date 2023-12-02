f43694c3444b31e6cf23ae58a11f433535c6bab0
Harris Khan <32182515+Harris-A-Khan@users.noreply.github.com>
2023-12-02 17:13:53 -0500

    chore(Resume.tsx): Apply formatting

--- frontend/src/Pages/Resume/Resume.tsx
+++ frontend/src/Pages/Resume/Resume.tsx
@@ -1,49 +1,48 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import ResumeDropzone from '../../components/Resume/ResumeDropzone';
-import { useUserStore } from '../../store/UserStore';
+import React, { useState } from "react";
+import axios from "axios";
+import ResumeDropzone from "../../components/Resume/ResumeDropzone";
+import { useUserStore } from "../../store/UserStore";
 import { toast } from "react-toastify";
-
 
 const Resume: React.FC = () => {
   // State to store the uploaded file
   const [file, setFile] = useState<File | null>(null);
-  
-  // The current resume data
-  const resumeName = useUserStore((state) => state.resume)
-  const userId = useUserStore((state) => state.id)
 
+  // The current resume data
+  const resumeName = useUserStore((state) => state.resume);
+  const userId = useUserStore((state) => state.id);
 
   const handleSubmit = async () => {
     if (file) {
       const formData = new FormData();
-      formData.append('resume', file);
-      formData.append('id', userId);
-  
+      formData.append("resume", file);
+      formData.append("id", userId);
+
       try {
-        const response = await axios.post('/users/uploadresume', formData, {
+        const response = await axios.post("/users/uploadresume", formData, {
           headers: {
-            'Content-Type': 'multipart/form-data',
+            "Content-Type": "multipart/form-data",
           },
         });
-  
+
         if (response.status === 201) {
-          console.log('Resume uploaded successfully');
-          toast.success("Resume Uploaded Successfully")
-          
+          console.log("Resume uploaded successfully");
+          toast.success("Resume Uploaded Successfully");
         }
       } catch (error) {
-        console.error('Error uploading the resume', error);
-        toast.error("Resume could not be uploaded")
+        console.error("Error uploading the resume", error);
+        toast.error("Resume could not be uploaded");
       }
     }
-  };  
+  };
 
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen">
         <div className="w-1/3">
-          <ResumeDropzone onFileUpload={(acceptedFiles) => setFile(acceptedFiles[0])} />
+          <ResumeDropzone
+            onFileUpload={(acceptedFiles) => setFile(acceptedFiles[0])}
+          />
           <button
             onClick={handleSubmit}
             className="px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded"
@@ -53,7 +52,11 @@ const Resume: React.FC = () => {
           {resumeName && (
             <div className="mt-4">
               <p>Current Resume: {resumeName}</p>
-              <a href={`/resumeviewer/${userId}`} target="_blank" rel="noopener noreferrer">
+              <a
+                href={`/resumeviewer/${userId}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 View
               </a>
             </div>
